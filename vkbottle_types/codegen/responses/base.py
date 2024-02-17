bf95f9d965f716a310a76479bdde638f45bde561
Vozdyx <vozdyx.dev@gmail.com>
2024-02-17 05:53:37 +0500

    Update base.py

--- vkbottle_types/codegen/responses/base.py
+++ vkbottle_types/codegen/responses/base.py
@@ -4,6 +4,7 @@ from vkbottle_types.responses.base_response import BaseResponse, BaseModel
 from vkbottle_types.base_model import Field
 
 from vkbottle_types.objects import (
+    BaseLinkNoProduct,
     BaseLinkButtonAction,
     BaseLinkApplicationStore,
     BaseLinkButtonActionType,
